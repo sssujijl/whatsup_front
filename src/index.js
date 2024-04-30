63e7c0dfb267442da224701f07489616d3cb153f
이수지 <leesuji1998@naver.com>
2024-04-30 12:28:44 +0900

    fix:baseURL

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 import { CookiesProvider } from 'react-cookie';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
